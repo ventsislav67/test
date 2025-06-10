9adb3afbeb85e9a1bf947b7114049dc81ae1225d
ventsislav67 <vencimkolev@gmail.com>
2025-06-10 10:17:45 +0300

    zadacha2

---
+++ test.10.06.25/test.10.06.25/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace test._10._06._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr3 = { 4, 3, 2, 36, 6, 12, 11, 23, 5, 8, 1, 7 };
+            int n3 = arr3.Length;
+
+            
+            for (int i = 1; i < n3; i++)
+            {
+                int key = arr3[i];
+                int j = i - 1;
+                while (j >= 0 && arr3[j] > key)
+                {
+                    arr3[j + 1] = arr3[j];
+                    j--;
+                }
+                arr3[j + 1] = key;
+            } 
+
+            Console.Write("Подреден масив: ");
+            Console.WriteLine(string.Join(" ", arr3));          
+            Console.Write("Най-дълга поредица от най-малкото: ");
+            int count = 1;
+            Console.Write(arr3[0] + " ");
+
+            for (int i = 1; i < n3; i++)
+            {
+                if (arr3[i] == arr3[i - 1] + 1)
+                {
+                    Console.Write(arr3[i] + " ");
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }          
+            Console.WriteLine();
+            Console.WriteLine("Дължина: " + count);
+        }
+    }
+}
